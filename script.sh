81605d034f402b085956c85167574d2ae7c67a79
alvaroficial <74686992+alvaroficial@users.noreply.github.com>
2021-10-14 23:52:59 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -s https://api.warframe.market/v1/items | jq ".[] | {payload: {items:[.[] | map(.) | .[] | {id:.id, item:.item_name}]}}" > items.json
+if [$? -eq 0]; then
+    echo "JSON CREADO CON EXITO"
+else
+    echo "ERROR"
+fi
